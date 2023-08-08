3f2d8c259e8f25446c5479938a7f2d65292ce9c0
jcmaker0627 <jcmaker0627@gmail.com>
2023-08-08 15:30:58 +0900

    favicon added

--- app/layout.js
+++ app/layout.js
@@ -15,11 +15,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ko">
       <body className={`${inter.className} bg-[#fafafa]`}>
-        <AuthProvider>
-          <LoadingPage />
-          <Navbar className="fixed top-0" />
-          {children}
-        </AuthProvider>
+          <AuthProvider>
+            <LoadingPage />
+            <Navbar className="fixed top-0" />
+            {children}
+          </AuthProvider>
       </body>
     </html>
   );
